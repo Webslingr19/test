fb74fc349cace3ed8bfa540d62d5ab0ddd095b63
Jason Sunnyassy <w0451497@nscc.ca>
2021-09-24 05:46:26 -0700

    added more basic Javascript

--- hello.js
+++ hello.js
@@ -1,1 +1,12 @@
 console.log("Hello World!")
+
+// user input
+
+// use an external library to get synchronous input from the user
+var prompt = require("prompt-sync")()
+
+// prompt user for input
+var name = prompt("Enter your name: ")
+
+// Use inputted value
+console.log(`Hi, ${name}!`)
