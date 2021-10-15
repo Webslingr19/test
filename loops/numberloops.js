db46f6ceb9e3db5e693ab0549dda2d676ff8171f
Jason Sunnyassy <w0451497@nscc.ca>
2021-10-15 06:24:19 -0700

    Added Arrays

--- loops/numberloops.js
+++ loops/numberloops.js
@@ -1,20 +1,24 @@
 var prompt = require("prompt-sync")()
 
 // ask the user to type in numbers until they are done
-var num = 0
-var end = ""
-while(end != "Y") {
-    num = prompt("Enter a number: ")
-    end = prompt("Enter 'Y' to end.")
-}
+// var num = 0
+// var end = ""
+// while(end != "Y") {
+//     num = prompt("Enter a number: ")
+//     end = prompt("Enter 'Y' to end.")
+// }
 
 // ask the user to type in numbers until they are done
-var num = 0
-var end = ""
-while(end != "Y") {
-    end = prompt("Enter 'Y' to enter a number: ")
-    if (end == "Y") {
-        num = prompt("Enter a number: ")
-        end=""
-    }
-}
+// var num = 0
+// var end = "Y"
+// while(end == "Y") {
+//     num = prompt("Enter a number: ")
+//     end = prompt("Enter 'Y' to enter a number: ")
+// }
+
+// ask the user to type in numbers until they are done
+
+do {
+    var num = prompt("Enter a number: ")
+    var end = prompt("Enter 'Y' to enter a number: ")
+} while(end == "Y")
